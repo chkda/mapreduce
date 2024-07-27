8664c5cf9f3924651b84d643d59c7301b2ea7585
Chhaya <chayakudas.das56@gmail.com>
2024-07-27 16:39:56 +0530

    Refactoring for thread safe map and reduce task

---
+++ master/mapTask.go
@@ -0,0 +1,78 @@
+package master
+
+import (
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type TaskStatus int
+
+const (
+	IDLE TaskStatus = iota
+	INPROGRESS
+	COMPLETE
+	FAILED
+)
+
+type MapTask struct {
+	ID          string
+	mu          sync.RWMutex
+	TaskFile    string
+	WorkerID    string
+	NumReduce   int
+	TaskStatus  TaskStatus
+	OutputFiles []string
+}
+
+func NewMapTask(taskFile string, workerId string, numReduce int) *MapTask {
+	return &MapTask{
+		ID:         uuid.NewString(),
+		TaskFile:   taskFile,
+		WorkerID:   workerId,
+		TaskStatus: IDLE,
+		NumReduce:  numReduce,
+	}
+}
+
+func (t *MapTask) GetTaskFile() string {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.TaskFile
+}
+
+func (t *MapTask) GetWorkerId() string {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.WorkerID
+}
+
+func (t *MapTask) GetNumReduce() int {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.NumReduce
+}
+
+func (t *MapTask) GetTaskStatus() TaskStatus {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.TaskStatus
+}
+
+func (t *MapTask) SetTaskStatus(status TaskStatus) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.TaskStatus = status
+}
+
+func (t *MapTask) GetOutputFiles() []string {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.OutputFiles
+}
+
+func (t *MapTask) SetOutputFiles(files []string) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.OutputFiles = files
+}
---
+++ master/reduceTask.go
@@ -0,0 +1,66 @@
+package master
+
+import (
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type ReduceTask struct {
+	ID         string
+	mu         sync.RWMutex
+	TaskFiles  []*ReduceDataNodeInfo
+	WorkerID   string
+	TaskStatus TaskStatus
+	OutputFile string
+}
+
+func NewReduceTask(taskFiles []*ReduceDataNodeInfo, workerId string) *ReduceTask {
+	return &ReduceTask{
+		ID:         uuid.NewString(),
+		TaskFiles:  taskFiles,
+		WorkerID:   workerId,
+		TaskStatus: IDLE,
+	}
+}
+
+func (t *ReduceTask) GetTaskFiles() []*ReduceDataNodeInfo {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.TaskFiles
+}
+
+func (t *ReduceTask) GetTaskStatus() TaskStatus {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.TaskStatus
+}
+
+func (t *ReduceTask) SetTaskStatus(status TaskStatus) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.TaskStatus = status
+}
+
+func (t *ReduceTask) GetWorkerId() string {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.WorkerID
+}
+
+func (t *ReduceTask) SetOutputFile(file string) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.OutputFile = file
+}
+
+func (t *ReduceTask) GetOutputFile() string {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.OutputFile
+}
+
+type ReduceDataNodeInfo struct {
+	Filename string
+	NodeIP   string
+}
